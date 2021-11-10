668e5f6c36834ba4b5b35642bc3bc1ac04eb10f0
Sarthak Taneja <sarthak.t@appdeskservices.com>
2021-11-10 17:20:34 +0530

    Pod file issue fix

--- ToDoListFinal/TableViewCell/RecordsTableViewCell.swift
+++ ToDoListFinal/TableViewCell/RecordsTableViewCell.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 
+protocol RecordsTableViewCellProtocol {
+    func deleteRecord(for record: Records)
+}
+
 class RecordsTableViewCell: UITableViewCell {
 
     // MARK: - IBOutlets
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    var currentRecord: Records?
+    var delegate: RecordsTableViewCellProtocol?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -28,7 +35,15 @@ class RecordsTableViewCell: UITableViewCell {
         guard let data = data else {
             return
         }
+        currentRecord = data
         dateLabel.text = convertDateToString(date: data.date ?? Date())
         titleLabel.text = data.title
+    }
+    
+    @IBAction func deleteButtonTap() {
+        guard let currentRecord = currentRecord else {
+            return
+        }
+        delegate?.deleteRecord(for: currentRecord)
     }
 }
